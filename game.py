fb95afa6de1b497ebdbeb0ba87044aab8c410250
gerardoamzvi <111869677+gerardoamzvi@users.noreply.github.com>
2026-03-07 09:27:30 +0100

    Started implementing check of piece pinned and several funtions that will be usefull later

--- game.py
+++ game.py
@@ -63,6 +63,38 @@ def is_movement_allowed(board, mov, pawn: Pawn, position):
     return False
 
 
+def King_position(board, color):
+    # Return the position of the king of the given color
+    for row in range(8):
+        for col in range(8):
+            piece = board[row][col]
+            if piece is not None and piece.get_type() == PawnType.ROI and piece.get_color() == color:
+                return (row, col)
+    return None
+
+def is_king_in_check(board, pawn, color, king_attacked_moves : list):
+    # Check if the king of the given color is in check
+    king_position = King_position(board, color)
+    if king_position is None:
+        return False
+
+     # The king has is in check
+    if king_position in king_attacked_moves:
+        return False 
+    
+def is_king_in_checkmate(board, pawn, color, king_attacked_moves : list, king_available_moves : list):
+    # Check if the king of the given color is in checkmate
+    if not is_king_in_check(board, pawn, color, king_attacked_moves):
+        return False
+
+    # Check if the king has any legal move to escape check
+    for move in king_available_moves:
+        if move not in king_attacked_moves:
+            return False
+
+    return True
+
+
 def is_path_clear(board, mov, position, pawn):
     # Vérifie si le chemin entre start et end est dégagé
     
@@ -96,7 +128,6 @@ def play_round(board, player, opponent, round_count):
         print(f"Au tour de Noir:")
 
     print("---------------------------------")
-    
     move = input("Entrez votre coup (ex: d4-e5): ")
 
     success = player.play(move)
@@ -104,3 +135,39 @@ def play_round(board, player, opponent, round_count):
     if not success:
         play_round(board, player, opponent, round_count)
 
+
+def King_available_moves(board, color, king_attacked_moves : list):
+    # Return a list of available moves for the king at the given position
+    available_moves = []
+
+    king_position = King_position(board, color)
+    if king_position is None:
+        return available_moves
+    for move in PawnType.ROI.value[2]:
+        new_row = king_position[0] + move[1]
+        new_col = king_position[1] + move[0]
+
+        if 0 <= new_row < 8 and 0 <= new_col < 8:
+            if board[new_row][new_col] is None and (new_row, new_col) not in king_attacked_moves:
+                available_moves.append((new_row, new_col))
+            elif board[new_row][new_col] is not None and board[new_row][new_col].get_color() != color and (new_row, new_col) not in king_attacked_moves:
+                available_moves.append((new_row, new_col))
+    return available_moves
+
+
+def King_attacked_moves(board, color):
+    # Return a list of moves that attack the king of the given color
+    attacked_moves = []
+
+    for row in range(8):
+        for col in range(8):
+            piece = board[row][col]
+            if piece is not None and piece.get_color() != color:
+                for move in piece.get_type().value[2]:
+                    new_row = row + move[1]
+                    new_col = col + move[0]
+
+                    if 0 <= new_row < 8 and 0 <= new_col < 8:
+                        attacked_moves.append((new_row, new_col))
+    
+    return attacked_moves
