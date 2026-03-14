de814c4c0840af625c754e9409c447948ee52f3f
gerardoamzvi <111869677+gerardoamzvi@users.noreply.github.com>
2026-03-14 13:33:13 +0100

    fix : merge confict fix

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
+from frontend import title
 from color import Color
 from game import play_round
+from history_helper import check_repetition, save_board
 from pawn import Pawn
 from pawn_type import PawnType
 from player import Player
 
+title()
+
 test_rock_board = [
-        [Pawn(PawnType.TOUR, Color.NOIR), Pawn(PawnType.CAVALIER, Color.NOIR), None, None, Pawn(PawnType.ROI, Color.NOIR), Pawn(PawnType.FOU, Color.NOIR), None, Pawn(PawnType.TOUR, Color.NOIR)],
-        [Pawn(PawnType.PION, Color.NOIR) for _ in range(8)],
-        [None for _ in range(8)],
-        [None for _ in range(8)],
-        [None for _ in range(8)],
-        [None for _ in range(8)],
-        [Pawn(PawnType.PION, Color.BLANC) for _ in range(8)],
-        [Pawn(PawnType.TOUR, Color.BLANC), None, None, None, Pawn(PawnType.ROI, Color.BLANC), None, None, Pawn(PawnType.TOUR, Color.BLANC)],
-    ]
+    [Pawn(PawnType.TOUR, Color.NOIR), Pawn(PawnType.CAVALIER, Color.NOIR), None, None, Pawn(PawnType.ROI, Color.NOIR), Pawn(PawnType.FOU, Color.NOIR), None, Pawn(PawnType.TOUR, Color.NOIR)],
+    [Pawn(PawnType.PION, Color.NOIR) for _ in range(8)],
+    [None for _ in range(8)],
+    [None for _ in range(8)],
+    [None for _ in range(8)],
+    [None for _ in range(8)],
+    [Pawn(PawnType.PION, Color.BLANC) for _ in range(8)],
+    [Pawn(PawnType.TOUR, Color.BLANC), None, None, None, Pawn(PawnType.ROI, Color.BLANC), None, None, Pawn(PawnType.TOUR, Color.BLANC)],
+]
+
 sequence_board = [
     [None, None, None, None, Pawn(PawnType.ROI, Color.NOIR), None, None, None],
     [None, None, None, Pawn(PawnType.PION, Color.NOIR), Pawn(PawnType.PION, Color.NOIR), Pawn(PawnType.PION, Color.NOIR), None, None],
@@ -21,11 +26,11 @@ sequence_board = [
     [None, None, None, None, None, None, None, None],
     [None, None, None, None, None, None, None, None],
     [None, None, None, None, None, None, None, None],
-    [None, None, None, None, None, None,  Pawn(PawnType.TOUR, Color.NOIR), None],
+    [None, None, None, None, None, None, Pawn(PawnType.TOUR, Color.NOIR), None],
     [None, None, None, None, Pawn(PawnType.ROI, Color.BLANC), None, None, Pawn(PawnType.DAME, Color.BLANC)],
 ]
-test_castling_board = [
 
+test_castling_board = [
     [Pawn(PawnType.TOUR, Color.NOIR), None, None, None, Pawn(PawnType.ROI, Color.NOIR), None, None, Pawn(PawnType.TOUR, Color.NOIR)],
     [None for _ in range(8)],
     [None, None, Pawn(PawnType.FOU, Color.NOIR), None, None, Pawn(PawnType.CAVALIER, Color.NOIR), None, None],
@@ -34,11 +39,9 @@ test_castling_board = [
     [None, None, Pawn(PawnType.FOU, Color.BLANC), None, None, Pawn(PawnType.CAVALIER, Color.BLANC), None, None],
     [None for _ in range(8)],
     [Pawn(PawnType.TOUR, Color.BLANC), None, None, None, Pawn(PawnType.ROI, Color.BLANC), None, None, Pawn(PawnType.TOUR, Color.BLANC)],
-
 ]
 
 test_check_board = [
-
     [Pawn(PawnType.TOUR, Color.NOIR), None, None, None, Pawn(PawnType.ROI, Color.NOIR), None, None, Pawn(PawnType.TOUR, Color.NOIR)],
     [None, Pawn(PawnType.PION, Color.NOIR), None, None, None, None, Pawn(PawnType.PION, Color.NOIR), None],
     [None, None, Pawn(PawnType.CAVALIER, Color.NOIR), None, None, Pawn(PawnType.FOU, Color.NOIR), None, None],
@@ -47,20 +50,8 @@ test_check_board = [
     [None, None, Pawn(PawnType.FOU, Color.BLANC), None, None, Pawn(PawnType.CAVALIER, Color.BLANC), None, None],
     [None, Pawn(PawnType.PION, Color.BLANC), None, None, None, None, Pawn(PawnType.PION, Color.BLANC), None],
     [Pawn(PawnType.TOUR, Color.BLANC), None, None, None, Pawn(PawnType.ROI, Color.BLANC), None, None, Pawn(PawnType.TOUR, Color.BLANC)],
-
 ]
 
-
-test_stalemate_board = [
-    [Pawn(PawnType.ROI, Color.NOIR), None, None, None, None, None, None, None],  # r0  roi noir en a0
-    [None, None, None, Pawn(PawnType.DAME, Color.BLANC), None, None, None, None], # r1  dame blanche en c1
-    [None, Pawn(PawnType.ROI, Color.BLANC), None, None, None, None, None, None],  # r2  roi blanc en b2
-    [None, None, None, None, None, None, None, None],
-    [None, None, None, None, None, None, None, None],
-    [None, None, None, None, None, None, None, None],
-    [None, None, None, None, None, None, None, None],
-    [None, None, None, None, None, None, None, None],
-]
 def init_board():
     board = [
         [Pawn(PawnType.TOUR, Color.NOIR), Pawn(PawnType.CAVALIER, Color.NOIR), Pawn(PawnType.FOU, Color.NOIR), Pawn(PawnType.DAME, Color.NOIR), Pawn(PawnType.ROI, Color.NOIR), Pawn(PawnType.FOU, Color.NOIR), Pawn(PawnType.CAVALIER, Color.NOIR), Pawn(PawnType.TOUR, Color.NOIR)],
@@ -72,14 +63,12 @@ def init_board():
         [Pawn(PawnType.PION, Color.BLANC) for _ in range(8)],
         [Pawn(PawnType.TOUR, Color.BLANC), Pawn(PawnType.CAVALIER, Color.BLANC), Pawn(PawnType.FOU, Color.BLANC), Pawn(PawnType.DAME, Color.BLANC), Pawn(PawnType.ROI, Color.BLANC), Pawn(PawnType.FOU, Color.BLANC), Pawn(PawnType.CAVALIER, Color.BLANC), Pawn(PawnType.TOUR, Color.BLANC)],
     ]
-
     return board
 
 
 if __name__ == "__main__":
-
-    #board = init_board()
-    board = test_stalemate_board
+    # board = init_board()
+    board = sequence_board
 
     player_white = Player(board, Color.BLANC)
     player_black = Player(board, Color.NOIR)
@@ -88,17 +77,25 @@ if __name__ == "__main__":
     round_count = 1
     run = True
     while run:
-        # TODO: Check win condition
         opponent = player_black if current_player.color == Color.BLANC else player_white
+
+        # Save board state for repetition draw
+        save_board(board, player_white, player_black, current_player)
+
+        # Check for repetion draw
+        if check_repetition():
+            print("==================================")
+            print("NUL par répétition !")
+            print("L'exact même position c'est répétée 3 fois sur l'échéquier.")
+            print("==================================")
+            break
 
         play_round(board, current_player, opponent, round_count)
         if current_player.king_in_checkmate:
             print(f"Checkmate! {opponent.color.value} wins!")
             run = False
             break
-        if current_player.stalemate:
-            print(f"Stalemate! {opponent.color.value} wins!")
-            run = False
-            break
         round_count += 1
-        current_player = player_black if current_player.color == Color.BLANC else player_white
+        current_player = (
+            player_black if current_player.color == Color.BLANC else player_white
+        )
